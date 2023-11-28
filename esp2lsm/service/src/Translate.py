2a35989e930cb0f3b72d5af002e9f86a0888d97f
artLara <art.lara.caz.com>
2023-11-28 01:43:35 -0600

    Change video format

--- esp2lsm/service/src/Translate.py
+++ esp2lsm/service/src/Translate.py
@@ -1,13 +1,14 @@
-
-
 from .Utils import Utils
 import cv2
-
+import os
+import time
 class Translate():
     def __init__(self):
         self.__vocab = Utils.createVocabulary()
         self.__path = '../raw/'
         self.__messageCount = 0
+        self.__pubicVideoPath = '/home/lara/Desktop/dactilologiaLSM_microservices/deafUI/videos_robot/'
+
 
     def extractFrames(self, file_name):
         vidcap = cv2.VideoCapture(self.__path + file_name)
@@ -21,11 +22,13 @@ class Translate():
     
     def writeVideo(self, frames):
         self.__messageCount += 1
-        dir_path = '/home/lara/Desktop/dactilologiaLSM_microservices/videos/'
+        # dir_path = '/home/lara/Desktop/dactilologiaLSM_microservices/deafUI/videos_robot/'
         frameSize = (640, 480)
-        out = cv2.VideoWriter(dir_path+'message{}.mp4'.format(self.__messageCount),cv2.VideoWriter_fourcc(*'MP4V'), 10, frameSize)
+        out = cv2.VideoWriter(self.__pubicVideoPath+'message{}_tmp.mp4'.format(self.__messageCount),cv2.VideoWriter_fourcc(*'mp4v'), 20.0, frameSize)
         for frame in frames:
-            out.write(frame)
+            # x = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            x = cv2.resize(frame, frameSize)
+            out.write(x)
         out.release()
 
     def finish(self):
@@ -45,13 +48,17 @@ class Translate():
 
         # Transform frames into video
         self.writeVideo(videoFrames)
+        videoName = self.__pubicVideoPath + 'message{}.mp4'.format(self.__messageCount)
+        tmp = self.__pubicVideoPath + 'message{}_tmp.mp4'.format(self.__messageCount)
+        os.system("ffmpeg -i {} -vcodec libx264 -f mp4 {}".format(tmp, videoName))
+        os.remove(tmp)
         return 'message{}.mp4'.format(self.__messageCount)
 
 
 
     def searchWords(self, message):
         words = []
-        for word in message:
+        for word in message.split():
             if self.isWordInVocab(word):
                 words.append(self.__vocab[word])
             else:
@@ -63,7 +70,7 @@ class Translate():
         spelling = []
         for letter in word:
             if self.isWordInVocab(letter):
-                spelling.appned(self.__vocab[letter])
+                spelling.append(self.__vocab[letter])
 
         return spelling
 
